a054b2af73d5de937d7d66cf7eb70ae3b4e8346d
Jacob Kerssemakers <>
2024-11-04 15:14:17 +0100

    Analysis/AutoStepfinder

---
+++ Analysis/AutoStepfinder/AutoStepfinder_links.m
@@ -0,0 +1,35 @@
+% Autostepfinder_links
+% LINKS
+% LATEST VERSION LINK: 
+    %https://zenodo.org/record/4657659
+    %DOI: 10.5281/zenodo.4657659
+% SUPPORTING PAPER & Manual: 
+    %DOI:https://doi.org/10.1016/j.patter.2021.100256
+
+% This is a reference script for:
+% AutoStepfinder: a fast and automated step detection method for single-molecule analysis
+    % 
+    % Luuk Loeff[1,2,3], Jacob W. J. Kerssemakers[1,3], Chirlmin Joo1 [4], Cees Dekker [1,4]
+    % 
+    %1 Kavli Institute of Nanoscience and Department of Bionanoscience, Delft University of Technology, Delft, The Netherlands 
+    %2  Present address: Department of Biochemistry, University of Zurich, Zurich, Switzerland 
+    %3 Equal contribution 
+    %4 %Correspondence: C.Joo@tudelft.nl (CJ), C.Dekker@tudelft.nl (CD)
+
+% HIGHLIGHTS
+    %1) Fast, automated, and bias-free detection of steps within single-molecule trajectories.
+    %2) Robust step detection without any prior knowledge on the data.
+    %3) A dual-pass strategy for the detection of steps over a wide variety of scales.
+    %4) A user-friendly interface for a simplified step fitting procedure.
+% 
+% REPOSITORY CONTENTS 
+    %1) code to analyze steps: 'AutoStepFinder' 
+    %2) code for cleaning data from corrupted points (such as Inf): 'DataDuster' 
+    %3) code for generating test traces : 'StepMaker' 
+    %4) zip file containing test traces
+% 
+% FOR USERS 
+    %1) an elaborate manual is included with Supplemental information in the paper 
+    %2) please cite the paper when using this code 
+    %3) for questions regarding this code, please contact: Dr. Jacob Kerssemakers (j.w.j.kerssemakers@tudelft.nl) or Dr. Luuk Loeff (l.loeff@bioc.uzh.ch)
+
